42729b37f1f2ea74512da913f6126ebe4d27428c
Bruno Bispo <brunobispoo25@gmail.com>
2022-10-06 21:48:42 -0300

    Jest 03 - Usando o spyOn.

---
+++ src/03-spyOn/README.md
@@ -0,0 +1,3 @@
+# Padrões para nomear classes
+
+Quando estamos testando classes, em um arquivo pode conter várias classes, e para sabermos qual é a principal, costumamos chama-la de **sut** (System Under Test) ou **cut** (Class Under Test)
---
+++ src/03-spyOn/persistency.spec.ts
@@ -0,0 +1,31 @@
+import { Persistency } from "../03-spyOn/persistency";
+
+describe("Test class Persistency", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Should return undefined", () => {
+    const sut = new Persistency();
+
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it("Should call console.log once", () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, "log");
+
+    sut.saveOrder();
+
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("Should call console.log with Pedido salvo com sucesso...", () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, "log");
+
+    sut.saveOrder();
+
+    expect(consoleSpy).toHaveBeenCalledWith("Pedido salvo com sucesso...");
+  });
+});
